402e7a0007cc8a7801e559a187e839ac894f31d8
Saugat Jung Bista <134186923+JungBista-Saugat@users.noreply.github.com>
2025-05-06 00:20:55 +0545

    Added member Order

--- Pustaksathi.API/Controllers/Application/Members/MemberController.cs
+++ Pustaksathi.API/Controllers/Application/Members/MemberController.cs
@@ -1,8 +1,125 @@
-﻿using Pustaksathi.API.Controllers.Shared.Auth;
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustaksathi.API.Controllers.Shared.Auth;
+using Pustaksathi.Interface.Application.Members;
+using Pustaksathi.Model.Application.Members;
+using Pustaksathi.Model.Shared.Param;
+using Pustaksathi.Model.Shared.Response;
+using Serilog;
 
 namespace Pustaksathi.API.Controllers.Application.Members
 {
     public class MemberController: AuthController
     {
+        private readonly IMembersService _membersService;
+        public MemberController(IMembersService context)
+        {
+            _membersService = context;
+        }
+
+        #region Members Orders
+        [HttpGet]
+        public async Task<IActionResult> GetOrdersByUserId([FromQuery]UserIdParam param) //saugat
+        {
+            Log.Information("===============================> GET: GetOrdersByUserId");
+            try
+            {
+                List<Orders>? response = await _membersService.OrderByUserIdSel(param);
+                if(response == null)
+                {
+                    return Ok(new ResponseModel<object>
+                    {
+                        Type = EnumResponse.Failed.ToString(),
+                        Message = "No Orders Found",
+                        Data = null
+                    });
+                }
+
+                return Ok(new ResponseModel<List<Orders>>
+                {
+                    Type = EnumResponse.Success.ToString(),
+                    Message = "Orders Found",
+                    Data = response
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseModel<object> { 
+                    Type = EnumResponse.SomethingWentWrong.ToString(), 
+                    Message = ex.Message.ToString(), 
+                    Data = null });
+            }
+        }
+
+        //Saugaut
+        [HttpPost]
+        public async Task<IActionResult>  OrderTsk([FromBody] Orders param)
+        {
+            Log.Information("===============================> POST: OrderTsk");
+            try
+            {
+                Orders? response = await _membersService.OrderTsk(param);
+                if (response == null)
+                {
+                    return Ok(new ResponseModel<object>
+                    {
+                        Type = EnumResponse.Failed.ToString(),
+                        Message = "No Orders Found",
+                        Data = null
+                    });
+                }
+                return Ok(new ResponseModel<Orders>
+                {
+                    Type = EnumResponse.Success.ToString(),
+                    Message = "Orders Found",
+                    Data = response
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseModel<object>
+                {
+                    Type = EnumResponse.SomethingWentWrong.ToString(),
+                    Message = ex.Message.ToString(),
+                    Data = null
+                });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CancelOrder([FromBody] OrderIdParam param) //saugat
+        {
+            Log.Information("===============================> POST: CancelOrder");
+            try
+            {
+                FlagResponse response = await _membersService.CancelOrder(param);
+                if (!response.IsSuccess)
+                {
+                    return Ok(new ResponseModel<object>
+                    {
+                        Type = EnumResponse.Failed.ToString(),
+                        Message = response.Message,
+                        Data = null
+                    });
+                }
+                return Ok(new ResponseModel<FlagResponse>
+                {
+                    Type = EnumResponse.Success.ToString(),
+                    Message = response.Message,
+                    Data = response
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseModel<object>
+                {
+                    Type = EnumResponse.SomethingWentWrong.ToString(),
+                    Message = ex.Message.ToString(),
+                    Data = null
+                });
+            }
+        }
+        #endregion
+
+        
     }
 }
--- Pustaksathi.Model/Application/Members/MembersModel.cs
+++ Pustaksathi.Model/Application/Members/MembersModel.cs
@@ -1,8 +1,31 @@
 ﻿
 namespace Pustaksathi.Model.Application.Members
 {
-    public record MembersDetails // Not fixed
-    {
-        public string Name { get; set; } = string.Empty;
-    }
+#region Members Orders
+  public record Orders
+  {
+      public Guid OrderId { get; set; }
+      public Guid UserId { get; set; }
+      public DateTime? OrderDate { get; set; }
+      public string? Status { get; set; }
+      public string? ClaimCode { get; set; }
+      public bool IsCancelled { get; set; }
+      public bool LoyalityDiscount { get; set; }
+      public bool QuantityDiscount { get; set; } = false;
+      public decimal TotalAmount { get; set; }
+      public DateTime? CreatedAt { get; set; }
+      public DateTime? ModifiedAt { get; set; }
+      public IReadOnlyCollection<OrderItems> OrderItems { get; set; } = new List<OrderItems>();
+  }
+
+  public record OrderItems
+  {
+      public Guid OrderItemId { get; set; }
+      public Guid OrderId { get; set; }
+      public Guid BookId { get; set; }
+      public string? BookTitle { get; set; }
+      public int Quantity { get; set; }
+      public decimal UnitPrice { get; set; }
+  }
+  #endregion
 }
