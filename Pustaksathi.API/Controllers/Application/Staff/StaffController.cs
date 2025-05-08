aa7a5747872f6465e92fc4f5c97c0045e41f62f0
GyanjuRai <raigyanju3@gmail.com>
2025-05-08 12:44:06 +0545

    refine staff claim code verifying feature are pull.

--- Pustaksathi.API/Controllers/Application/Staff/StaffController.cs
+++ Pustaksathi.API/Controllers/Application/Staff/StaffController.cs
@@ -1,8 +1,57 @@
-﻿using Pustaksathi.API.Controllers.Shared.Auth;
+﻿using E2_Dynamics.Model.Shared.Enum;
+using Microsoft.AspNetCore.Mvc;
+using Pustaksathi.API.Controllers.Shared.Auth;
+using Pustaksathi.Interface.Application.Staff;
+using Pustaksathi.Model.Application.Members;
+using Pustaksathi.Model.Shared.Param;
+using Pustaksathi.Model.Shared.Response;
+using Serilog;
 
 namespace Pustaksathi.API.Controllers.Application.Staff
 {
     public class StaffController: AuthController
     {
+        private readonly IStaffService _staffService;
+
+        public StaffController(IStaffService context)
+        {
+            _staffService = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CheckClaimCode([FromQuery] OrderClaimCodeParam param)
+        {
+            Log.Information("===============================> GET: CheckClaimCode");
+            try
+            {
+                List<Orders>? response = await _staffService.OrderByClaimCodeSel(param);
+                if (response == null)
+                {
+                    return NotFound(new ResponseModel<object>
+                    {
+                        Type = EnumResponse.NoRecordFound.ToString(),
+                        Message = "No Order Found",
+                        Data = null
+                    });
+                }
+
+                return Ok(new ResponseModel<List<Orders>>
+                {
+                    Type = EnumResponse.Success.ToString(),
+                    Message = "Order Found",
+                    Data = response
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseModel<object>
+                {
+                    Type = EnumResponse.SomethingWentWrong.ToString(),
+                    Message = "Something went wrong",
+                    Data = null,
+                    Exception = ex
+                });
+            }
+        }
     }
 }
--- Pustaksathi.Interface/Application/Staff/IStaffService.cs
+++ Pustaksathi.Interface/Application/Staff/IStaffService.cs
@@ -1,8 +1,16 @@
 ﻿
 
+using Pustaksathi.Model.Application.Members;
+using Pustaksathi.Model.Shared.Param;
+
 namespace Pustaksathi.Interface.Application.Staff
 {
     public interface IStaffService
     {
+        /// <summary>
+        /// Get Orders By Claim code for staff
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<Orders>?> OrderByClaimCodeSel(OrderClaimCodeParam param); // Saugat bista
     }
 }
